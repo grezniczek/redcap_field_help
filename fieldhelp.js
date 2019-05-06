bc8e0e640dd155f80ce5f0c39a1a09b6ffc6a1f5
G. Rezniczek <g.rezniczek@gmail.com>
2019-05-06 14:12:32 +0200

    Find unnamed toggle targets within same table row.

--- fieldhelp.js
+++ fieldhelp.js
@@ -2,17 +2,24 @@ $(function() {
     const toggles = $('[data-toggle-toggle]')
     toggles.bind('click', function() {
         const toggle = $(this)
+        // Fix for href.
+        if (this.hasAttribute('href')) {
+            this.setAttribute('href','javascript:;')
+        }
         const name = toggle.attr('data-toggle-toggle')
+        // Set up for named targets.
         if (name != undefined && name.length > 0) {
             $(`[data-toggle-target=${name}]`).each(function() {
                 $(this).toggle()
             })
         }
+        // Set up for unnamed (within same table row).
         else {
-            toggle.siblings('[data-toggle-target]').each(function() {
+            toggle.closest('tr').find('[data-toggle-target]').each(function() {
                 $(this).toggle()
             })
         }
+        return false
     })
     const targets = $('[data-toggle-target]')
     targets.each(function() {
